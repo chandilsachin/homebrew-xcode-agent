b802571740e5b631bc11debeef39733166cc5a7b
Sachin Chandil <chandilsachin@gmail.com>
2026-07-02 23:32:06 +0530

    Update beta formula to v0.2.0-rc.1

--- Formula/xcode-agent-beta.rb
+++ Formula/xcode-agent-beta.rb
@@ -1,12 +1,12 @@
 class XcodeAgentBeta < Formula
   desc "Agent-friendly CLI for Xcode/iOS development (release candidate)"
   homepage "https://github.com/chandilsachin/xcode-agent"
-  version "0.1.0-rc.1"
+  version "0.2.0-rc.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/chandilsachin/xcode-agent/releases/download/v0.1.0-rc.1/xcode-agent-0.1.0-arm64-apple-macosx.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/chandilsachin/xcode-agent/releases/download/v0.2.0-rc.1/xcode-agent-0.2.0-arm64-apple-macosx.tar.gz"
+      sha256 "68a88ff737c2c698fea09dce85c94af99334b9db4a7541f2513587aa6dce6e60"
     end
   end
 
