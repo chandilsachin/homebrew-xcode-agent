dc449cc826b036a032883fb8424ef4a5896e9fb3
Sachin Chandil <sachinchandil@Sachins-MacBook-Pro.local>
2026-07-10 21:17:30 +0530

    xcode-agent-beta: v0.2.0-rc.5

--- Formula/xcode-agent-beta.rb
+++ Formula/xcode-agent-beta.rb
@@ -1,12 +1,12 @@
 class XcodeAgentBeta < Formula
   desc "Agent-friendly CLI for Xcode/iOS development (release candidate)"
   homepage "https://github.com/chandilsachin/xcode-agent"
-  version "0.2.0-rc.4"
+  version "0.2.0-rc.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/chandilsachin/xcode-agent/releases/download/v0.2.0-rc.4/xcode-agent-0.2.0-arm64-apple-macosx.tar.gz"
-      sha256 "92a93d7e832958202ed846734e9d227418308d33516187a00284eb93cf6c59ab"
+      url "https://github.com/chandilsachin/xcode-agent/releases/download/v0.2.0-rc.5/xcode-agent-0.2.0-arm64-apple-macosx.tar.gz"
+      sha256 "e5c193f5b19d99a006c30660b3d70c4132d7e8d71de8b20768876bf5819f365f"
     end
   end
 
